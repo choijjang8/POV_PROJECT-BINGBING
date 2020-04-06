23f1dcce6c4ef496d9e79fbf5695c389ec034d2f
choi gi hwan <choijjang8@withsystem.co.kr>
2020-04-06 22:49:21 +0900

    Make bing-bing project
    POV 프로젝트 생성
    
    Signed-off-by: choi gi hwan <choijjang8@withsystem.co.kr>

---
+++ BING_BING_PROJECT.ino
@@ -0,0 +1,32 @@
+// Visual Micro is in vMicro>General>Tutorial Mode
+// 
+/*
+    Name:       BING_BING_PROJECT.ino
+    Created:	2020-04-06 오후 10:44:35
+    Author:     DESKTOP-E0NLNHT\gigih
+*/
+
+// Define User Types below here or use a .h file
+//
+
+
+// Define Function Prototypes that use User Types below here or use a .h file
+//
+
+
+// Define Functions below here or use other .ino or cpp files
+//
+
+// The setup() function runs once each time the micro-controller starts
+void setup()
+{
+
+
+}
+
+// Add the main program code into the continuous loop() function
+void loop()
+{
+
+
+}
